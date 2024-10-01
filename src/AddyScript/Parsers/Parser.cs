4e601f1b6817295e9aac7e645756db4b1a2a429e
Michel Mbem <michel.mbem@gmail.com>
2024-09-30 22:05:45 -0400

    A bit of code cleaning

--- src/AddyScript/Parsers/Parser.cs
+++ src/AddyScript/Parsers/Parser.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.ExceptionServices;
+
 using AddyScript.Ast;
 using AddyScript.Ast.Expressions;
 using AddyScript.Ast.Statements;
