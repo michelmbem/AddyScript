b6ff092d8bb6d825e7d582f84f384e102ee19ce5
Michel Mbem <michel.mbem@gmail.com>
2025-11-24 21:52:29 -0500

    Refined the about dialog box

--- src/AddyScript.Gui/AboutBox.axaml.cs
+++ src/AddyScript.Gui/AboutBox.axaml.cs
@@ -8,6 +8,11 @@ public partial class AboutBox : Window
     public AboutBox()
     {
         InitializeComponent();
+        
+        TitleTextBlock.Text = AssemblyInfo.Title;
+        VersionTextBlock.Text = $"Version {AssemblyInfo.Version}";
+        DescriptionTextBlock.Text = AssemblyInfo.Description;
+        CopyrightTextBlock.Text = AssemblyInfo.Copyright;
     }
 
     private void OkButtonClick(object sender, RoutedEventArgs e)
---
+++ src/AddyScript.Gui/AssemblyInfo.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Reflection;
+
+
+namespace AddyScript.Gui;
+
+
+internal static class AssemblyInfo
+{
+    internal static Assembly ExecutingAssembly => Assembly.GetExecutingAssembly();
+
+    internal static string Version => ExecutingAssembly.GetName().Version!.ToString();
+
+    internal static T GetAssemblyAttribute<T>()
+    {
+        var attributes = ExecutingAssembly.GetCustomAttributes(typeof(T), false);
+        if (attributes.Length <= 0) return default;
+        return (T)attributes[0];
+    }
+
+    internal static string Title
+    {
+        get
+        {
+            var titleAttribute = GetAssemblyAttribute<AssemblyTitleAttribute>();
+            return titleAttribute != null
+                ? titleAttribute.Title
+                : Path.GetFileNameWithoutExtension(ExecutingAssembly.Location);
+        }
+    }
+
+    internal static string Description
+    {
+        get
+        {
+            var descriptionAttribute = GetAssemblyAttribute<AssemblyDescriptionAttribute>();
+            return descriptionAttribute != null ? descriptionAttribute.Description : string.Empty;
+        }
+    }
+
+    internal static string Copyright
+    {
+        get
+        {
+            var copyrightAttribute = GetAssemblyAttribute<AssemblyCopyrightAttribute>();
+            return copyrightAttribute != null ? copyrightAttribute.Copyright : string.Empty;
+        }
+    }
+
+    internal static string Company
+    {
+        get
+        {
+            var companyAttribute = GetAssemblyAttribute<AssemblyCompanyAttribute>();
+            return companyAttribute != null ? companyAttribute.Company : string.Empty;
+        }
+    }
+}
