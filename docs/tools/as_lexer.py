ad0a55de9e155bd64a16f4083197c2df92acde71
Michel Mbem <michel.mbem@gmail.com>
2026-02-03 01:34:51 -0500

    Doc fix

--- docs/tools/as_lexer.py
+++ docs/tools/as_lexer.py
@@ -102,10 +102,6 @@ class AddyScriptLexer(RegexLexer):
         # Strings
         # --------------------
         "strings": [
-            (r"\$@'", String, "interpolated-verbatim-string-single"),
-            (r'\$@"', String, "interpolated-verbatim-string-double"),
-            (r"\$'", String, "interpolated-string-single"),
-            (r'\$"', String, "interpolated-string-double"),
             (r"@'", String, "verbatim-string-single"),
             (r'@"', String, "verbatim-string-double"),
             (r"'", String, "string-single"),
@@ -139,65 +135,6 @@ class AddyScriptLexer(RegexLexer):
             (r'"', String, "#pop"),
             (r'[^"]+', String),
             (r'.', String),
-        ],
-
-        # --------------------
-        # Interpolated Strings
-        # --------------------
-        "interpolated-string-single": [
-            (r"'", String, "#pop"),
-            (r'{{', String.Escape),
-            (r'}}', String.Escape),
-            (r'{', String.Interpol, "interpolation"),
-            (r"\\['\\abfnrtv0]", String.Escape),
-            (r"[^'\\{]+", String),
-            (r'.', String),
-        ],
-
-        "interpolated-string-double": [
-            (r'"', String, "#pop"),
-            (r'{{', String.Escape),
-            (r'}}', String.Escape),
-            (r'{', String.Interpol, "interpolation"),
-            (r'\\["\\abfnrtv0]', String.Escape),
-            (r'[^"\\{]+', String),
-            (r'.', String),
-        ],
-
-        "interpolated-verbatim-string-single": [
-            (r"''", String),
-            (r"'", String, "#pop"),
-            (r'{{', String.Escape),
-            (r'}}', String.Escape),
-            (r'{', String.Interpol, "interpolation"),
-            (r"[^'{]+", String),
-            (r'.', String),
-        ],
-
-        "interpolated-verbatim-string-double": [
-            (r'""', String),
-            (r'"', String, "#pop"),
-            (r'{{', String.Escape),
-            (r'}}', String.Escape),
-            (r'{', String.Interpol, "interpolation"),
-            (r'[^"{]+', String),
-            (r'.', String),
-        ],
-
-        # --------------------
-        # Interpolation Expression
-        # --------------------
-        "interpolation": [
-            (r'}', String.Interpol, "#pop"),
-            include("whitespace"),
-            include("comments"),
-            include("keywords"),
-            include("types"),
-            include("numbers"),
-            include("strings"),
-            include("operators"),
-            include("punctuation"),
-            include("identifiers"),
         ],
 
         # --------------------
