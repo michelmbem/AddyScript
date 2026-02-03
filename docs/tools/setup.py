fe293c653f233caae25d8113eb514e8ee2ecbe67
Michel Mbem <michel.mbem@gmail.com>
2026-02-02 21:32:15 -0500

    Doc update

--- docs/tools/setup.py
+++ docs/tools/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="addyscript-lexer",
-    py_modules=["addyscript_lexer"],
+    py_modules=["as_lexer"],
     entry_points={
         'pygments.lexers': [
-            'AddyScript = addyscript_lexer:AddyScriptLexer',
-            'addyscript = addyscript_lexer:AddyScriptLexer',
-            'add = addyscript_lexer:AddyScriptLexer',
+            'AddyScript = as_lexer:AddyScriptLexer',
+            'addyscript = as_lexer:AddyScriptLexer',
+            'add = as_lexer:AddyScriptLexer',
         ],
     },
 )
