ba0eb530013c9d426883781856505e2d16dbba7c
Michel Mbem <michel.mbem@gmail.com>
2026-02-15 11:06:55 -0500

    Updated AssemblyInfo.cs files in Gui and Interactive

--- src/AddyScript.Interactive/AssemblyInfo.cs
+++ src/AddyScript.Interactive/AssemblyInfo.cs
@@ -1,56 +1,25 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace AddyScript.Interactive;
 
 internal static class AssemblyInfo
 {
+    public static string Title => GetAssemblyAttribute<AssemblyTitleAttribute>()?.Title ??
+                                  Path.GetFileNameWithoutExtension(ExecutingAssembly.Location);
+
+    public static string Description => GetAssemblyAttribute<AssemblyDescriptionAttribute>()?.Description;
+
+    public static string Version => ExecutingAssembly.GetName().Version?.ToString();
+
+    public static string Copyright => GetAssemblyAttribute<AssemblyCopyrightAttribute>()?.Copyright;
+
+    public static string Company => GetAssemblyAttribute<AssemblyCompanyAttribute>()?.Company;
+
     private static Assembly ExecutingAssembly => Assembly.GetExecutingAssembly();
 
-    public static string Version => ExecutingAssembly.GetName().Version!.ToString();
-
-    private static T GetAssemblyAttribute<T>()
-    {
-        var attributes = ExecutingAssembly.GetCustomAttributes(typeof(T), false);
-        if (attributes.Length <= 0) return default;
-        return (T)attributes[0];
-    }
-
-    public static string Title
-    {
-        get
-        {
-            var titleAttribute = GetAssemblyAttribute<AssemblyTitleAttribute>();
-            return titleAttribute != null
-                ? titleAttribute.Title
-                : Path.GetFileNameWithoutExtension(ExecutingAssembly.Location);
-        }
-    }
-
-    public static string Description
-    {
-        get
-        {
-            var descriptionAttribute = GetAssemblyAttribute<AssemblyDescriptionAttribute>();
-            return descriptionAttribute != null ? descriptionAttribute.Description : string.Empty;
-        }
-    }
-
-    public static string Copyright
-    {
-        get
-        {
-            var copyrightAttribute = GetAssemblyAttribute<AssemblyCopyrightAttribute>();
-            return copyrightAttribute != null ? copyrightAttribute.Copyright : string.Empty;
-        }
-    }
-
-    public static string Company
-    {
-        get
-        {
-            var companyAttribute = GetAssemblyAttribute<AssemblyCompanyAttribute>();
-            return companyAttribute != null ? companyAttribute.Company : string.Empty;
-        }
-    }
+    private static T GetAssemblyAttribute<T>() => ExecutingAssembly.GetCustomAttributes(typeof(T), false)
+                                                                   .Cast<T>()
+                                                                   .FirstOrDefault();
 }
